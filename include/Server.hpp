e619ebb53e2030e6a82ffe8729846e8a1e7634a3
Håvard Olai Solbakken <haavaoso@stud.ntnu.no>
2024-10-22 19:00:10 +0200

    Fix network helper morten

--- include/Server.hpp
+++ include/Server.hpp
@@ -6,81 +6,38 @@
 #define SERVER_HPP
 
 #include "simple_socket/TCPSocket.hpp"
+#include "network_helper.hpp"
+#include <vector>
+#include <string>
 
-class Server {
-    public:
-    explicit Server(const simple_socket::TCPServer &server) : server_(server) {
 
+class Server : public simple_socket::TCPServer {
+public:
+    explicit Server(int port) : TCPServer(port), port_(port) {
     }
 
+    void recieveData(const std::unique_ptr<simple_socket::SimpleConnection> &connection, std::vector<unsigned char> &data) {
+        std::vector<unsigned char> sizeBuffer(4); // get msg size
+        connection->readExact(sizeBuffer);
 
+        const auto size = bytes_to_int(sizeBuffer);
+        std::vector<unsigned char> Buffer(size);
+        connection->readExact(Buffer);
+        data = Buffer;
+    };
+    void sendData(const std::unique_ptr<simple_socket::SimpleConnection> &connection, const std::vector<unsigned char> &data) {
+        std::vector<unsigned char> imgBuffer;
+        auto bytes = int_to_bytes(imgBuffer.size());
+        std::vector toSend(imgBuffer.begin(), imgBuffer.end());
+        toSend.insert(toSend.begin(), bytes.begin(), bytes.end());
+
+        connection->write(toSend);
+    }
 
 private:
-    const simple_socket::TCPServer& server_;
+    int port_;
 };
 
-/*
 
-#include <iostream>
-#include <ostream>
-#include <opencv2/opencv.hpp>
-#include "simple_socket/TCPSocket.hpp"
-#include "network_helper.hpp"
-
-
-
-void receiveData(const std::unique_ptr<simple_socket::SocketConnection> &connection, std::vector<unsigned char> &data) {
-    std::vector<unsigned char> sizeBuffer(4); // get msg size
-    connection->readExact(sizeBuffer);
-
-    const auto size = bytes_to_int(sizeBuffer);
-    std::vector<unsigned char> Buffer(size);
-    connection->readExact(Buffer);
-    data = Buffer;
-}
-
-void sendData(const std::unique_ptr<simple_socket::SocketConnection> &connection, const std::vector<unsigned char> &data) {
-    std::vector<unsigned char> imgBuffer;
-    auto bytes = int_to_bytes(imgBuffer.size());
-    std::vector toSend(imgBuffer.begin(), imgBuffer.end());
-    toSend.insert(toSend.begin(), bytes.begin(), bytes.end());
-
-    connection->write(toSend);
-
-}
-
-int main() {
-    std::string input;
-
-    std::string ip = "10.53.98.136";
-    int port = 9090;
-    simple_socket::TCPClientContext client;
-    const auto conn = client.connect(ip, port);
-    bool running = true;
-
-    std::jthread clientThread([&] {
-        std::cin >> input;
-        if (input == "q") {
-            conn->close();
-            running = false;
-        }
-    });
-
-    while(running) {
-        std::vector<unsigned char> data;
-        receiveData(conn, data);
-        Mat image = imdecode(data, IMREAD_COLOR);
-
-        auto rect = faceDetect(image);
-        drawFaces(image, rect);
-
-        std::vector<unsigned char> imgBuffer;
-        //std::vector<int> params = {cv::IMWRITE_JPEG_QUALITY, 90};
-        imencode(".jpg", image, imgBuffer);
-        sendData(conn, imgBuffer);
-    }
-    return 0;
-}
-*/
 
 #endif //SERVER_HPP
--- main.cpp
+++ main.cpp
@@ -3,12 +3,40 @@
 //
 
 #include <iostream>
-
+#include "Server.hpp"
 
 
 int main() {
     for (int i = 99; i < 103; i++){
     std::cerr << "main" << std::endl;
     }
+    std::string ip = "localhost";
+    int port = 9090;
+
+    Server server(port);
+    std::cout << "Server is running and waiting for a client connection on port " << port << "..." << std::endl;
+
+    auto ok = server.accept();
+
+    std::cout << "Client connected!" << std::endl;
+
+    std::vector<unsigned char> receivedData;
+
+    server.recieveData(ok,receivedData);
+
+    std::string data(receivedData.begin(), receivedData.end() );
+    std::cout << "Received from client: " << data << std::endl;
+
+
+
+    std::string response = "I have aaaaaaaaa monster!";
+    std::vector<unsigned char> responseData(response.begin(), response.end());
+    server.sendData(ok, responseData);
+
+
+    ok->close();
+
     return 21;
 }
+
+
